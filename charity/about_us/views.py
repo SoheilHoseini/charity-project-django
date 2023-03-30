a4c5e0c5a0bdc575788339f8bf24e3ffb87542f5
Soheil Hoseini <soheil.hosseini78@gmail.com>
2023-03-31 00:46:24 +0430

    modify about us page

--- charity/about_us/views.py
+++ charity/about_us/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from accounts.models import User
+
+
 def about_us(request):
-    # View for about-us page
-    ...
+    allUsers = User.objects.all().values()
+    context = {"users": allUsers}
+    #return HttpResponse(str(allUsers.get_full_name()))
+    return render(request, 'about_us.html', context=context)
+
--- charity/charity/urls.py
+++ charity/charity/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from about_us.views import about_us
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("about-us/", include("about_us.urls")),
 ]
