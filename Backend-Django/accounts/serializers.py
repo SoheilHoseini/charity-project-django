06c40c432cac97ad7d72baf1644b3382ca0fc5be
Soheil Hoseini <soheil.hosseini78@gmail.com>
2023-04-18 18:33:43 +0430

    create new user

--- Backend-Django/accounts/serializers.py
+++ Backend-Django/accounts/serializers.py
@@ -4,4 +4,17 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'phone',
+                  'address', 'gender', 'age', 'description',
+                  'first_name', 'last_name', 'email']
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+
--- Backend-Django/accounts/views.py
+++ Backend-Django/accounts/views.py
@@ -3,7 +3,6 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .serializers import UserSerializer
 
 
@@ -18,5 +17,13 @@ class LogoutAPIView(APIView):
         )
 
 
-class UserRegistration(APIView):
-    pass
+class UserRegistration(generics.CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(status=200,
+                data={"message": "User added successfully!"})
+
+        return Response({"message": user_serializer.errors})
+
